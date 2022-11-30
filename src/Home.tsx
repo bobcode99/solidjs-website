c3092b65b88477c3214f5d2cfdb84504096c8f22
yb <lojobs38@gmail.com>
2022-11-30 21:20:42 +0800

    Update app

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,28 +4,28 @@ import logo from './logo.svg';
 import styles from './App.module.css';
 
 const Home: Component = () => {
-    // return (
-    //     <div class={styles.App}>
-    //       <header class={styles.header}>
-    //         <img src={logo} class={styles.logo} alt="logo" />
-    //         <p>
-    //           Edit <code>src/App.tsx</code> and save to reload.
-    //         </p>
-    //         <a
-    //           class={styles.link}
-    //           href="https://github.com/solidjs/solid"
-    //           target="_blank"
-    //           rel="noopener noreferrer"
-    //         >
-    //           Learn Solid
-    //         </a>
-    //       </header>
-    //     </div>
-    //   );
-
     return (
-      <div>This site was made with Solid</div>
-    )
+        <div class={styles.App}>
+          <header class={styles.header}>
+            <img src={logo} class={styles.logo} alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+            </p>
+            <a
+              class={styles.link}
+              href="https://github.com/solidjs/solid"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn Solid
+            </a>
+          </header>
+        </div>
+      );
+
+    // return (
+    //   <div>This site was made with Solid</div>
+    // )
 }
 export default Home;
 
