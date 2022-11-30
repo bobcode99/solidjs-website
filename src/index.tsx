5e1738552d73c40d544f8386dadf38d80b72c8c0
yb <lojobs38@gmail.com>
2022-11-30 20:44:08 +0800

    Using normal div

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
 /* @refresh reload */
 import { render } from 'solid-js/web';
 
-import './index.css';
-import App from './App';
-import { Router } from '@solidjs/router';
+// import './index.css';
+// import App from './App';
+// import { Router } from '@solidjs/router';
+import Home from './Home';
 
-render(() => <Router><App /></Router> 
+// render(() => <Router><App /></Router> 
+
+// , document.getElementById('root') as HTMLElement);
+
+render(() => <Home/>
 
 , document.getElementById('root') as HTMLElement);
