b605117ea35bd47ad419fb962732b348274d2873
yb <lojobs38@gmail.com>
2022-12-01 12:34:57 +0800

    add deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,17 +7,19 @@ set -e
 pnpm run build
 
 # navigate into the build output directory
-cd dist
+# cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > .nojekyll
+echo > ./dist/nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
 # git init
 # git checkout -B main
-git add -A
+
+git add dist -f
+# git add -A
 git commit -m 'for deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
