8dcaea0f9bc936bbac299a9056a8e8e97ed14756
yb <lojobs38@gmail.com>
2022-12-01 12:49:46 +0800

    update deploy script

--- deploy.sh
+++ deploy.sh
@@ -7,10 +7,10 @@ set -e
 pnpm run build
 
 # navigate into the build output directory
-# cd dist
+cd dist
 
 # place .nojekyll to bypass Jekyll processing
-echo > ./dist/nojekyll
+echo > .nojekyll
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -18,8 +18,8 @@ echo > ./dist/nojekyll
 # git init
 # git checkout -B main
 
-git add dist -f
-# git add -A
+# git add dist -f
+git add -A
 git commit -m 'for deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
@@ -27,6 +27,6 @@ git commit -m 'for deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-# git push -f git@github.com:bobcode99/solidjs-website.git main:gh-pages
-git subtree push --prefix dist origin gh-pages
+git push -f git@github.com:bobcode99/solidjs-website.git main:gh-pages
+# git subtree push --prefix dist origin gh-pages
 cd -
