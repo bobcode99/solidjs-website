edb22489bf420a34a1af8a94cb34683edc90bc38
yb <lojobs38@gmail.com>
2022-12-01 13:06:57 +0800

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,10 @@ echo > .nojekyll
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
 
-# git init
-# git checkout -B main
-
-# git add dist -f
-git add . -f
-git commit -m 'for deploy'
+git init
+git checkout -B main
+git add -A
+git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
